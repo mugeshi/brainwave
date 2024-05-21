b8b24f48759aed9c028c7047f1ac4bb598af0e40
mugeshi <nicolemugeshi@gmail.com>
2024-05-21 22:49:26 +0300

    updated useLocation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
+import { useLocation } from "react-router-dom"; 
 
 function Header() {
+  const { pathname } = useLocation(); 
+
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:px-4">
@@ -15,7 +18,13 @@ function Header() {
               <a
                 key={item.id}
                 href={item.url}
-                className="block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 lg:mx-4 my-2 lg:my-0"
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 lg:mx-4 my-2 lg:my-0 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname 
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
               </a>
